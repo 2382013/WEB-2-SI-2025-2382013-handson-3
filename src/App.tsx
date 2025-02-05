0f9f8edeeaab4c830494228fdf8e3d8c11ae29be
yessica <2382013@unai.edu>
2025-02-05 11:59:53 +0700

    handson 3 commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <p style={{color: "red"}}>Hello world</p>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Nim</th>
+            <th>Address</th>
+            <th>Email</th>
+          </tr>
+          <tr>
+            <td>Helena Onezia Lewi</td>
+            <td>2381003</td>
+            <td>Unai</td>
+            <td>2381003@unai.edu</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App
